4afcb5694f13db2cfdc22c50a3956942da0b80d8
PedroEid <38227797+PedroEid@users.noreply.github.com>
2018-05-04 10:19:23 -0300

    Revert "a"
    This reverts commit c33355cd3c686537bc1b28c6aca935848d425a94.

--- J0g0.py
+++ J0g0.py
@@ -1,33 +1,31 @@
 import pygame
+import sys
+from pygame.locals import *
+from random import randrange
+#criando classe bebe
+class Bebe (pygame.sprite.Sprite):
+    def __init__(self, arquivo_imagem, pos_x, pos_y):
+        self.image = pygame.image.load(arquivo_imagem)
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = pos_y
 
-def jogo():
-    pygame.init()
-    tela = pygame.display.set_mode([400,400])
-    pygame.display.set_caption("Bem vindo ao jogo")
-    fps = pygame.time.Clock()
-    mamadeira = pygame.image.load("mamadeira.jpg")
-    fundo = pygame.image.load("fundi1.jpg")
-    bebe = pygame.image.load("bbbravo.jpg")
-    
-    def bebep(x,y):
-        tela.blit(bebe,(x,y))
+#criando tela
+pygame.init()
+tela = pygame.display.set_mode([800,600])
+pygame.display.set_caption("Bem vindo ao jogo")
+fundo = pygame.image.load("fundi1.jpg").convert()
+bebe=Bebe('bbbravo.jpg',randrange(40),randrange(40))
+bebe_group = pygame.sprite.Group()
+sair = False
+while sair!= True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sair = False     
+    tela.blit(fundo, (0, 0))
+    bebe_group.draw(tela)
+    pygame.display.update()
         
-    def municao(x,y):
-        tela.blit(mamadeira,(x,y))
-
-    def fundo1(x,y):
-        tela.blit(fundo(x,y))
-    
-    sair = False
-    while sair!= True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sair = True 
-        fps.tick(30)     
-        pygame.display.update()
-        
-    pygame.quit()
+pygame.display.quit()
     
     
-    
-jogo()
