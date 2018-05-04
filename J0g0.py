0661d45a12029b1afc182f9fa265a7286bd49064
PedroEid <38227797+PedroEid@users.noreply.github.com>
2018-05-04 11:39:57 -0300

    a

--- J0g0.py
+++ J0g0.py
@@ -6,12 +6,19 @@ from random import randrange
 class Bebe (pygame.sprite.Sprite):
     def __init__(self, imbebe, pos_x, pos_y):
         pygame.sprite.Sprite.__init__(self)
-#        imbebe=pygame.transform.scale(imbebe,(40,40))
         self.image = pygame.image.load(imbebe)
+        self.image=pygame.transform.scale(self.image,(200,200))
         self.rect = self.image.get_rect()
         self.rect.x = pos_x
         self.rect.y = pos_y
-
+class Mamadeira (pygame.sprite.Sprite):
+    def __init__(self, immadeira, pos_x, pos_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(immadeira)
+        self.image=pygame.transform.scale(self.image,(1,1))
+        self.rect = self.image.get_rect()
+        self.rect.x = pos_x
+        self.rect.y = pos_y
 #criando tela
 pygame.init()
 tela = pygame.display.set_mode([800,600])
@@ -19,16 +26,26 @@ pygame.display.set_caption("Bem vindo ao jogo")
 fundo = pygame.image.load("fundi1.jpg").convert()
 fundo=pygame.transform.scale(fundo,(800,600))
 
-bebe=Bebe('bbbravo.jpg',randrange(700),randrange(500))
+#informacoes classes
+
+pygame.display.set_caption('Baby Fight')
+m_normal=Bebe('mamadeira.png',randrange(100),randrange(600))
+bebe=Bebe('bbbravo.jpg',randrange(100),randrange(600))
+
+#grupos
+
 bebe_group = pygame.sprite.Group()
 bebe_group.add(bebe)
+mamadeira_group = pygame.sprite.Group()
+mamadeira_group.add(m_normal)
 sair = False
 while sair!= True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sair = False     
+            sair = True     
     tela.blit(fundo, (0,0))
     bebe_group.draw(tela)
+    mamadeira_group.draw(tela)
     pygame.display.update()
         
 pygame.display.quit()
