434cb747f87149441503d574de86975c409342ef
PedroEid <38227797+PedroEid@users.noreply.github.com>
2018-05-06 14:07:04 -0300

    multiplos bebes

--- J0g0.py
+++ J0g0.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 from random import randrange
 #cores
 white=(255,255,255)
+gray=(125,125,125)
 black=(0,0,0)
 red=(255,0,0)
 #criando classe bebe
@@ -15,38 +16,51 @@ class Bebe (pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = pos_x
         self.rect.y = pos_y
+    
 class Mamadeira (pygame.sprite.Sprite):
-    def __init__(self, immadeira, pos_x, pos_y):
+    def __init__(self, immadeira, pos_x, pos_y,vel_x,vel_y,g):
+        self.vx=vel_x
+        self.vy=vel_y
+        self.g=g
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(immadeira)
         self.image=pygame.transform.scale(self.image,(20,20))
         self.rect = self.image.get_rect()
         self.rect.x = pos_x
         self.rect.y = pos_y
-#criando tela
+    def move(self):
+        self.rect.x+=self.vx
+        self.rect.y+=self.vy+ 1/2*self.g
+    #criando tela
 pygame.init()
 tela = pygame.display.set_mode([800,600])
 pygame.display.set_caption("Bem vindo ao jogo")
-
-
-bebe=Bebe('bbbravo.jpg',100,300)
-m_normal=Mamadeira('mamadeira.png',181,355)
-#criando grupos
+bebe=[]
 bebe_group = pygame.sprite.Group()
 mamadeira_group=pygame.sprite.Group()
-bebe_group.add(bebe)
-mamadeira_group.add(m_normal)
+for i in range(randrange(2,4)):
+    z=randrange(700)
+    bebe+=[Bebe('bbbravo.jpg',z,300)]
+    bebe_group.add(bebe[i])
+    m_normal=Mamadeira('mamadeira.png',(z+81),355,10,(-10),(8))
+#criando grupos
+    mamadeira_group.add(m_normal)
+#Relogio
+relogio=pygame.time.Clock()
 sair = False
 while sair!= True:
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
-            sair = True     
+            sair = True 
+        if event.type == pygame.KEYDOWN:
+            if event.key== pygame.K_BACKSPACE:
+                m_normal.move()    
     tela.fill(white)
     bebe_group.draw(tela)
     mamadeira_group.draw(tela)
     pygame.display.update()
-        
+    relogio.tick(10)
 pygame.display.quit()
     
     
